e10add6b202a0915c61b6f22b311f7891dfee2da
rodrigocam <rodrigo.redcode@gmail.com>
2018-10-23 23:18:07 -0300

    Changes

--- src/gif-component.js
+++ src/gif-component.js
@@ -1,46 +1,54 @@
 
-function test () {
-    console.log('test')
-}
+// var self = null;
 
 AFRAME.registerComponent('gif', {
     schema: {
         src: { type: 'string', default: '' },
         marker: { default: '' },
+        position: { default: '' },
+        rotation: { default: '' },
     },
 
     init: function() {
-        this.isPlaying = false;
+        this.gifPlaying = false;
         this.gif = fetch(this.data.src)
-                    .then(resp => resp.arrayBuffer())
-                    .then(buff => new GIF(buff));
+        .then(resp => resp.arrayBuffer())
+        .then(buff => new GIF(buff));
         
         this.frames = this.gif.then(gif => gif.decompressFrames(true))
         this.canvas = document.createElement('canvas');
 
+        this.el.setAttribute('draw-canvas', this.canvas)
+        this.el.setAttribute('material', {src: this.canvas})
+        this.el.setAttribute('rotation', '270 0 0')
+        this.el.setAttribute('position', '0 0 0')
+
         this.marker = document.querySelector(this.data.marker);
+        var self = this        
         
         this.marker.addEventListener('markerFound', function(e) {
-            this.data.test();
+            console.log('found')
+            self.playGIF()
         })
 
         this.marker.addEventListener('markerLost', function(e) {
-            this.stopGIF();
+            console.log('lost')
+            self.stopGIF()
         })
     },
 
     playGIF : function() {
-        this.isPlaying = true;
+        this.gifPlaying = true;
         this.frames.then(frames => this.renderGIF(frames, 0));
         // this.render(this.frames, 0);
     },
 
     stopGIF : function() {
-        this.isPlaying = false;
+        this.gifPlaying = false;
     },
 
     renderGIF : function(frames, frameIndex) {
-        if(this.isPlaying) {
+        if(this.gifPlaying) {
             console.log('rendering')
             if (frameIndex >= frames.length) {
                 frameIndex = 0;
@@ -62,10 +70,11 @@ AFRAME.registerComponent('gif', {
         
             var timeDiff = end - start;
         
+            var self = this
             // setTimeout(this.render(frames, frameIndex+1),
                 // Math.max(0, Math.floor(frame.delay - timeDiff)))
             setTimeout(function() {
-                this.render(frames, frameIndex+1);
+                self.renderGIF(frames, frameIndex+1);
             }, Math.max(0, Math.floor(frame.delay - timeDiff)));
         }
     },
@@ -75,8 +84,8 @@ AFRAME.registerComponent('gif', {
         var tmpCtx = tmpCanvas.getContext('2d');
         var mainCtx = this.canvas.getContext('2d');
 
-        tmpCanvas.width = frame.dims.width;
-        tmpCanvas.height = frame.dims.height;
+        this.canvas.width = tmpCanvas.width = frame.dims.width;
+        this.canvas.height = tmpCanvas.height = frame.dims.height;
 
         var frameImageData = tmpCtx.createImageData(frame.dims.width, frame.dims.height);
         frameImageData.data.set(frame.patch);
